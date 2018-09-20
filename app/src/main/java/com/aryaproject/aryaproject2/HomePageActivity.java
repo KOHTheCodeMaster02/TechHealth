5f6a657001ddc0e349222e5d1bea2e8f232e4b17
King Of HearTs <kohthecodemaster02@gmail.com>
2018-09-20 08:59:46 +0530

    Commit : 5 Branch: CurrentProgess_2 Working: Syncing with Toast msg. Firebase File Storage Connectivity Successful. SignUp } Login | Database (user details) 20th September 2K18, 0812.

--- app/src/main/java/com/aryaproject/aryaproject2/HomePageActivity.java
+++ app/src/main/java/com/aryaproject/aryaproject2/HomePageActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
@@ -25,11 +26,9 @@ public class HomePageActivity extends AppCompatActivity implements View.OnClickL
 
         findViewById(R.id.idButtonSync).setOnClickListener(this);
 
-        //sync1();
     }
-
-    private void sync1() {
-
+/*
+    private void temp1() {
         storage = FirebaseStorage.getInstance();
         storageReference = storage.getReferenceFromUrl("gs://aryaproject2-7252e.appspot.com/test1").child("test.txt");
         stream = getResources().openRawResource(R.raw.ab);
@@ -37,9 +36,29 @@ public class HomePageActivity extends AppCompatActivity implements View.OnClickL
 
         Toast.makeText(getApplicationContext(), "Data Synced Successfully!",
                 Toast.LENGTH_SHORT).show();
+    }*/
+
+    private void sync1() {
+
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReferenceFromUrl("gs://aryaproject2-7252e.appspot.com/test1").child("test.txt");
+        stream = getResources().openRawResource(R.raw.ab);
+        uploadTask = storageReference.putStream(stream);
+        uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                msg1();
+            }
+        });
+
+
 
     }
 
+    private void msg1(){
+        Toast.makeText(getApplicationContext(), "Data Synced Successfully!",
+                Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     public void onClick(View v) {
